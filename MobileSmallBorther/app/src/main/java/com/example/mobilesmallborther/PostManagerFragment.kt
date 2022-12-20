fc162a5790c9f627e788ebc04ead9609fbe593fb
Pascal Vanmalderen <pascal.vanmalderen@hotmail.be>
2022-12-20 20:02:04 +0100

    Fetch From db

--- MobileSmallBorther/app/src/main/java/com/example/mobilesmallborther/PostManagerFragment.kt
+++ MobileSmallBorther/app/src/main/java/com/example/mobilesmallborther/PostManagerFragment.kt
@@ -1,59 +1,46 @@
 package com.example.mobilesmallborther
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+import com.example.mobilesmallborther.databinding.FragmentPostManagerBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [PostManagerFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class PostManagerFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var viewModel: PostManagerViewModel
+    private lateinit var binding: FragmentPostManagerBinding
+    private lateinit var listPostFragment: ListPostFragment
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
+        binding = FragmentPostManagerBinding.inflate(layoutInflater, container, false)
+        viewModel = ViewModelProvider(this).get(PostManagerViewModel::class.java)
+        listPostFragment = childFragmentManager.findFragmentByTag("postList") as ListPostFragment
+
+        // Nous observons la liste
+        viewModel.mutableLiveDataListPost.observe(viewLifecycleOwner) {
+            Log.i("test",it.toString())
+            listPostFragment.replacePostList(it)
+        }
+
+        viewModel.mutableLiveDataCreatePost.observe(viewLifecycleOwner) {
+            listPostFragment.addPost(it)
+        }
+
+        // Nous lançons la requête
+        viewModel.launchFetchAllPost()
+
         return inflater.inflate(R.layout.fragment_post_manager, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment PostManagerFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            PostManagerFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(PostManagerViewModel::class.java)
+        // TODO: Use the ViewModel
     }
 }

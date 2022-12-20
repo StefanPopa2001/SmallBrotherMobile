218a533e54a5c2d9bb20c9d61864bb1b232d26eb
Pascal Vanmalderen <pascal.vanmalderen@hotmail.be>
2022-12-20 18:46:52 +0100

    Add Depedencys and gradle

--- MobileSmallBorther/app/src/main/java/com/example/mobilesmallborther/MainActivity.kt
+++ MobileSmallBorther/app/src/main/java/com/example/mobilesmallborther/MainActivity.kt
@@ -2,8 +2,12 @@ package com.example.mobilesmallborther
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.mobilesmallborther.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)

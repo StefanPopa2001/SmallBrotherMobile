71dd9658b27bb996a2dad8cd270dd12aeddf9d22
Pascal Vanmalderen <pascal.vanmalderen@hotmail.be>
2022-12-20 18:25:45 +0100

    Add Activity

--- MobileSmallBorther/app/src/main/java/com/example/mobilesmallborther/MainActivity.kt
+++ MobileSmallBorther/app/src/main/java/com/example/mobilesmallborther/MainActivity.kt
@@ -1,11 +1,35 @@
 package com.example.mobilesmallborther
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: MainActivity
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        enableListeners()
+    }
+    private fun enableListeners() {
+        // Evenement afin de passer d'une fenêtre à une autre
+        binding.btnMainActivityFeed.setOnClickListener {
+            val intent = Intent(this, InteractivityActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.btnMainActivityGoToProfil.setOnClickListener {
+            val intent = Intent(this, FormActivity::class.java)
+            startActivity(intent)
+        }
+
+        /*binding.btnMainActivityGoToImplicit.setOnClickListener {
+            val intent = Intent(this, ImplicitActivity::class.java)
+            startActivity(intent)
+        }*/
+
     }
 }
